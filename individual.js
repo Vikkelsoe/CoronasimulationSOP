e68a1b17b40af57302e08e8ca1304c81fb1ee622
Christian Vikkelsø <chri21i5@edu.nextkbh.dk>
2020-11-02 20:45:18 +0100

    Forbedret simulationstype 2

--- individual.js
+++ individual.js
@@ -1,37 +1,9 @@
 class Individual {
   constructor(type) {
     if (simulType == 1) {
-      this.positionX = random(5, canvasSize - 5);
-      this.positionY = random(5, canvasSize - 5);
-      this.directionX = random(-5, 5);
-      this.directionY = random(-5, 5);
+      this.constructDirPos1();
     } else if (simulType == 2) {
-      this.positionX = random(5, canvasSize - 5);
-      if (this.positionX >= 200 && this.positionX <= 300) {
-        randomNum = random();
-        if (randomNum >= 0.5) {
-          this.positionY = random(5, 200);
-        } else {
-          this.positionY = random(300, canvasSize - 5);
-        }
-      } else {
-        this.positionY = random(5, canvasSize - 5);
-      }
-
-      this.destinationX = random(205, 296);
-      this.destinationY = random(205, 296);
-
-      this.directionX = (this.destinationX - this.positionX) * 0.01;
-      this.directionY = (this.destinationY - this.positionY) * 0.01;
-
-      this.headingIn = true;
-
-      this.StartX = this.positionX;
-      this.StartY = this.positionY;
-
-      //disse to linjer er nødvendige så this.positionX og Y ikke er lig this.startX og Y i starten. I så fald ville de bevæge sig væk fra midten fra start
-      this.positionX = this.positionX + this.directionX;
-      this.positionY = this.positionY + this.directionY;
+      this.constructDirPos2();
     }
 
     this.d = 10;
@@ -74,87 +46,93 @@ class Individual {
 
   move() {
     if (simulType == 1) {
-      if (
-        this.positionX - this.d / 2 <= 0 ||
-        this.positionX + this.d / 2 >= canvasSize
-      ) {
-        this.directionX = -this.directionX;
-      }
-
-      if (
-        this.positionY - this.d / 2 <= 0 ||
-        this.positionY + this.d / 2 >= canvasSize
-      ) {
-        this.directionY = -this.directionY;
-      }
+      this.move1();
     } else if (simulType == 2) {
-      if (this.headingIn) {
-        if (this.directionX > 0 && this.directionY > 0) {
-          if (
-            this.positionX >= this.destinationX &&
-            this.positionY >= this.destinationY
-          ) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = false;
-          }
-        } else if (this.directionX < 0 && this.directionY > 0) {
-          if (
-            this.positionX <= this.destinationX &&
-            this.positionY >= this.destinationY
-          ) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = false;
-          }
-        } else if (this.directionX > 0 && this.directionY < 0) {
-          if (
-            this.positionX >= this.destinationX &&
-            this.positionY <= this.destinationY
-          ) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = false;
-          }
-        } else if (this.directionX < 0 && this.directionY < 0) {
-          if (
-            this.positionX <= this.destinationX &&
-            this.positionY <= this.destinationY
-          ) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = false;
-          }
-        }
-      } else {
-        if (this.directionX > 0 && this.directionY > 0) {
-          if (this.positionX >= this.StartX && this.positionY >= this.StartY) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = true;
-          }
-        } else if (this.directionX < 0 && this.directionY > 0) {
-          if (this.positionX <= this.StartX && this.positionY >= this.StartY) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = true;
-          }
-        } else if (this.directionX > 0 && this.directionY < 0) {
-          if (this.positionX >= this.StartX && this.positionY <= this.StartY) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = true;
-          }
-        } else if (this.directionX < 0 && this.directionY < 0) {
-          if (this.positionX <= this.StartX && this.positionY <= this.StartY) {
-            this.directionX = -this.directionX;
-            this.directionY = -this.directionY;
-            this.headingIn = true;
-          }
-        }
-      }
+      this.move2();
     }
     this.positionX = this.positionX + this.directionX;
     this.positionY = this.positionY + this.directionY;
+  }
+
+  constructDirPos1() {
+    this.positionX = random(5, canvasSize - 5);
+    this.positionY = random(5, canvasSize - 5);
+    this.directionX = random(-5, 5);
+    this.directionY = random(-5, 5);
+  }
+
+  constructDirPos2() {
+    this.positionX = random(5, canvasSize - 5);
+
+    if (this.positionX >= 190 && this.positionX <= 310) {
+      randomNum = random();
+      if (randomNum >= 0.5) {
+        this.positionY = random(5, 190);
+      } else {
+        this.positionY = random(310, canvasSize - 5);
+      }
+    } else {
+      this.positionY = random(5, canvasSize - 5);
+    }
+
+    this.destinationX = random(205, 296);
+    this.destinationY = random(205, 296);
+
+    this.randomMultiplier = random(0.01, 0.02);
+    this.directionX =
+      (this.destinationX - this.positionX) * this.randomMultiplier;
+    this.directionY =
+      (this.destinationY - this.positionY) * this.randomMultiplier;
+
+    this.StartX = this.positionX;
+    this.StartY = this.positionY;
+
+    this.headingIn = true;
+
+    this.distanceToLastGoal =
+      ((this.StartX - this.positionX) ** 2 +
+        (this.StartY - this.positionY) ** 2) **
+      0.5;
+
+    this.travelDistance =
+      ((this.destinationX - this.StartX) ** 2 +
+        (this.destinationY - this.StartY) ** 2) **
+      0.5;
+  }
+
+  move1() {
+    if (
+      this.positionX - this.d / 2 <= 0 ||
+      this.positionX + this.d / 2 >= canvasSize
+    ) {
+      this.directionX = -this.directionX;
+    }
+
+    if (
+      this.positionY - this.d / 2 <= 0 ||
+      this.positionY + this.d / 2 >= canvasSize
+    ) {
+      this.directionY = -this.directionY;
+    }
+  }
+
+  move2() {
+    if (this.headingIn) {
+      this.distanceToLastGoal =
+        ((this.StartX - this.positionX) ** 2 +
+          (this.StartY - this.positionY) ** 2) **
+        0.5;
+    } else {
+      this.distanceToLastGoal =
+        ((this.destinationX - this.positionX) ** 2 +
+          (this.destinationY - this.positionY) ** 2) **
+        0.5;
+    }
+
+    if (this.distanceToLastGoal >= this.travelDistance) {
+      this.directionX = -this.directionX;
+      this.directionY = -this.directionY;
+      this.headingIn = !this.headingIn;
+    }
   }
 }
